d152ea9385c155267fcc9dc8886d7ceb0ba53ad4
Daniel Fernandez <mdanif9728@gmail.com>
2024-06-04 15:53:20 -0700

    assignment complete

---
+++ src/ColorBlock.js
@@ -0,0 +1,11 @@
+
+function ColorBlock(props) {
+  return (
+    <div className="colorBlock"
+      style={{ 'backgroundColor': props.color }}>
+      <p>{props.color}</p>
+    </div>
+  )
+}
+
+export default ColorBlock
